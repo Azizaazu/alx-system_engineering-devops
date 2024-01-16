d0953fa6b2e8c16569dc99092e9d7a9956fd1e6f
Azizaazu <aynaou.aziza98@gmail.com>
2024-01-16 14:48:15 -0800

    update

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,10 +6,11 @@ def number_of_subscribers(subreddit):
     """function that queries the Reddit API"""
 
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    headers = {'User-Agent': 'My User Agent 1.0'}
-    response = requests.get(url, headers=headers)
-    if response.status_code == 200:
-        data = response.json()
-        return data.get('data').get('subscribers')
-    else:
-        return 0
+    headers = {'User-Agent': 'request'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code != 200:
+        return (0)
+
+    data = response.json().get('data')
+    num_subs = data.get('subscribers')
+    return (num_subs)
